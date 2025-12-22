05a8be6ad34bb827a01687a9ad1fa5091fe51c90
leticia-noh <leticianhonda@gmail.com>
2025-12-22 15:21:06 -0300

    feat: add class Conteudo and inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso("Curso Java", "Descrição do curso Java", 8);
+
+        Curso curso2 = new Curso("Curso JS", "Descrição do curso JS", 4);
+
+        Mentoria mentoria = new Mentoria("Mentoria de Java", "Descrição da mentoria Java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
 
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,33 +1,15 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-
-    private String titulo;
-
-    private String descricao;
+public class Curso extends Conteudo {
 
     private int cargaHoraria;
 
     public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
+        super(titulo, descricao);
         this.cargaHoraria = cargaHoraria;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public Curso() {
     }
 
     public int getCargaHoraria() {
@@ -39,10 +21,15 @@ public class Curso {
     }
 
     @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
